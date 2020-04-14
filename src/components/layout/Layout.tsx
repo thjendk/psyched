fcca633f473cb110df26399d211c871b54f44eac
Thomas Jensen <44089656+thomasjensen1194@users.noreply.github.com>
2020-04-14 16:07:24 +0200

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,7 +7,7 @@ import styled from 'styled-components';
 export const MainLayout = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
+  min-height: 100vh;
 `;
 
 export const ContentContainer = styled.div`
